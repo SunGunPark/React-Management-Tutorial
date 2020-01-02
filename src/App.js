e177263d572e77ade4734ac77c76ca86cd6eec6b
SunGunPark <44153017+SunGunPark@users.noreply.github.com>
2020-01-02 14:05:02 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '홍길동',
+  'birthday' : '951011',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '이민수',
+  'birthday' : '981225',
+  'gender' : '남자',
+  'job' : '고등학생'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '제이나',
+  'birthday' : '930817',
+  'gender' : '여자',
+  'job' : '프로그래머'
+},
+{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '홍길동',
+  'birthday' : '951011',
+  'gender' : '남자',
+  'job' : '대학생'
+}
+]
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>개발을 시작해봅시다!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
+    
   );
 }
 
