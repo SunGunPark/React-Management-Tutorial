62d43e6d00955d840e56f1452f1abf60a7a6b05c
SunGunPark <44153017+SunGunPark@users.noreply.github.com>
2020-01-06 16:16:59 +0900

    implement Customer Add Module & Customer List Refresh Module

--- server.js
+++ server.js
@@ -11,21 +11,32 @@ const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: conf.host,
-    user: conf.user,
-    password: conf.password,
-    port: conf.port,
-    database: conf.database
-})
+const connection = mysql.createConnection(
+    {host: conf.host, user: conf.user, password: conf.password, port: conf.port, database: conf.database}
+)
+connection.connect();
+
+const multer = require('multer');
+const upload = multer({dest: './upload'})
 
 app.get('/api/customers', (req, res) => {
-    connection.query(
-        "SELECT * FROM CUSTOMER",
-        (err, rows, fields) => {
-            res.send(rows);
-        }
-    );
+    connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+        res.send(rows);
+    });
 });
+app.use('/image', express.static('./upload'));
 
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params, (err, rows, fields) => {
+        res.send(rows);
+        console.log(err);
+    });
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
