dd745165710ac2faf904c9451b7ff221f199f9d0
SunGunPark <44153017+SunGunPark@users.noreply.github.com>
2020-01-06 13:21:52 +0900

    Create Customer Database Table & Node.js Database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,38 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday': '951011',
-            'gender': '남자',
-            'job': '대학생'
-        }, {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '이민수',
-            'birthday': '981225',
-            'gender': '남자',
-            'job': '고등학생'
-        }, {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '제이나',
-            'birthday': '930817',
-            'gender': '여자',
-            'job': '프로그래머'
-        }, {
-            'id': 4,
-            'image': 'https://placeimg.com/64/64/4',
-            'name': '가로쉬',
-            'birthday': '850101',
-            'gender': '남자',
-            'job': '전사'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
